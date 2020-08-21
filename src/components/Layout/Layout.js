ef679a88f89737f75d9f5e30e2b687a8e93ab94b
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-08-21 15:46:15 +0200

    need to fix logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Layout.css'
+import SideDrawer from '../Navigation//SideDrawer/SideDrawer'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = ( props ) => (
     <>
-        <Toolbar/>
+        <Toolbar />
+        <SideDrawer/>
         <main className='Content'>
             {props.children}
         </main>
